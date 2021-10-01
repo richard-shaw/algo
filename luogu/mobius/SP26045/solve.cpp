e350f296326ba06131eaee05d98a504457e124c7
Richard <xiaoqiande01@qiyi.com>
2021-10-02 00:14:17 +0800

    t

--- luogu/mobius/SP26045/solve.cpp
+++ luogu/mobius/SP26045/solve.cpp
@@ -1,14 +1,17 @@
 //
 // Created by Shaw on 2021/9/20.
 //
-
+#pragma GCC optimize("Ofast", "inline", "-ffast-math")
+#pragma GCC target("avx,sse2,sse3,sse4,mmx")
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define MAXN 50004
+#define MAXN 1000004
 #define MOD 1000000007
-
+#define EPS 0.000000000000001
+#define _div(n, i) (int)(n * dv[i])
 int visit[MAXN], prime[MAXN], phi[MAXN], tot = 0;
+double dv[MAXN];
 void init() {
     memset(visit, 0, sizeof(visit));
     phi[1] = 1;
@@ -27,14 +30,18 @@ void init() {
     for (int i = 2; i < MAXN; i++) {
         phi[i] = (phi[i] + phi[i - 1]) % MOD;
     }
+    for (int i = 1; i < MAXN; i++) {
+        dv[i] = 1. / i * (1 + EPS);
+    }
 }
 
 int S(int N, int M) {
     int Ans = 0;
     if (N > M) swap(N, M);
     for (int i = 1; i <= N; ) {
-        int j = min(N, min(N / (N / i), M / (M / i)));
-        int Q = ((N / i) * (M / i)) % MOD;
+        int n0 = _div(N, i), m0 = _div(M, i);//N / i, m0 = M / i;
+        int j = min(N, min(_div(N, n0), _div(M, m0)));
+        int Q = (n0 * m0) % MOD;
         Ans = (Ans + (Q * (phi[j] - phi[i - 1])) % MOD) % MOD;
         i = j + 1;
     }
@@ -42,12 +49,14 @@ int S(int N, int M) {
     return Ans;
 }
 
-signed main() {
-    init();
+void solve() {
     int T, N, M, i1, i2, j1, j2;
     cin >> T >> N >> M;
+//    N = M = 1e6;
+//    T = 5e4;
     while (T--) {
         cin >> i1 >> j1 >> i2 >> j2;
+//        i1 = j1 = i2 = j2 = 1e6;
         if (i1 > i2 || j1 > j2) {
             cout << 0 << endl;
             continue;
@@ -56,4 +65,12 @@ signed main() {
         while (Ans < 0) Ans += MOD;
         cout << Ans << endl;
     }
+}
+
+signed main() {
+    init();
+//    clock_t t1 = clock();
+    solve();
+//    clock_t t2 = clock();
+//    cout << (double) (t2 - t1) / CLOCKS_PER_SEC << endl;
 }
